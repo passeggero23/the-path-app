b9e9eb7c10a662b832eb408529db0c5c4e079818
passeggero23 <passeggero23@gmail.com>
2026-01-07 17:00:04 +0100

    Initialize Flutter app with basic structure

---
+++ main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('The Path')),
+        body: Center(child: Text('Benvenuto su The Path: Nudge-Learning!')),
+      ),
+    ));
